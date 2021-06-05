c402f08fa4106a0e92032cad4a394ea89ddea218
Joana Brito <joanafiorentini@hotmail.com>
2021-06-04 20:18:18 -0700

    Add the event listeners and global variables

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ var starWhiteImg = document.getElementsByName('star-white');
 var starRedImg = document.getElementsByName('star-red');
 var xWhiteImg = document.getElementsByName('x-white');
 var xRedImg = document.getElementsByName('x-red');
+
+var savedIdeas =[];
+
+starredIdeasBtn.addEventListener("click", function());
+saveIdeaBtn.addEventListener("click", function());
+favoriteBtn.addEventListener("click", function());
+deleteCardBtn.addEventListener("click", function());
+commentBtn.addEventListener("click", function());
+starWhiteImg.addEventListener("click", function());
+starRedImg.addEventListener("click", function());
+xWhiteImg.addEventListener("click", function());
+xRedImg.addEventListener("click", function());
+.
+.
+.
+var newIdeaCard = new Idea(parameters:infoTitle, infoBody);
